15d5951e215866540eb6ead529e5045cdee51e79
yegow <allanyego05@gmail.com>
2020-05-18 18:16:19 +0300

    FT configured heroku deployment

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const pg = require('pg');
 require('pg-camelcase').inject(pg);
 
 let pool;
-const { NODE_ENV, PROD_DATABASE_URL } = process.env;
-if (NODE_ENV === 'production' && PROD_DATABASE_URL) {
+const { NODE_ENV, DATABASE_URL } = process.env;
+if (NODE_ENV === 'production' && DATABASE_URL) {
   pool = new pg.Pool({
-    connectionString: PROD_DATABASE_URL,
+    connectionString: DATABASE_URL,
     ssl: true,
   });
 } else {
