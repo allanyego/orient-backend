389392a01cdffa0a1736c7b9b076f0bbad5f2ece
yegow <allanyego05@gmail.com>
2020-06-01 15:37:20 +0300

    Fix

--- app.js
+++ app.js
@@ -26,33 +26,33 @@ app.use((err, req, res, next) => {
   res.render("error", { error: err });
 });
 
-if (process.env.NODE_ENV === "production") {
-  const CronJob = require("cron").CronJob;
-  const { getExpired } = require("./controllers/policies");
-  const { sendExpiryReminder } = require("./util/email/index");
+// if (process.env.NODE_ENV === "production") {
+//   const CronJob = require("cron").CronJob;
+//   const { getExpired } = require("./controllers/policies");
+//   const { sendExpiryReminder } = require("./util/email/index");
 
-  // Schedule job for everyday at 10am
-  const job = new CronJob("00 00 10 * * *", async function () {
-    try {
-      const expireds = await getExpired();
+//   // Schedule job for everyday at 10am
+//   const job = new CronJob("00 00 10 * * *", async function () {
+//     try {
+//       const expireds = await getExpired();
 
-      expireds.forEach(async (exp) => {
-        const { email, ...rest } = exp;
-        await sendExpiryReminder({
-          message: {
-            to: email,
-            subject: "Policy Expiration.",
-          },
-          locals: rest,
-        });
-      });
-    } catch (e) {
-      // TODO: implement robust logging
-      console.log("Problem sending email:", e);
-    }
-  });
+//       expireds.forEach(async (exp) => {
+//         const { email, ...rest } = exp;
+//         await sendExpiryReminder({
+//           message: {
+//             to: email,
+//             subject: "Policy Expiration.",
+//           },
+//           locals: rest,
+//         });
+//       });
+//     } catch (e) {
+//       // TODO: implement robust logging
+//       console.log("Problem sending email:", e);
+//     }
+//   });
 
-  job.start();
-}
+//   job.start();
+// }
 
 module.exports = app;
